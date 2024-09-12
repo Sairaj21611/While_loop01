77e17507fb914819a6e479641080495c277c9ecd
Sairaj21611 <159284736+Sairaj21611@users.noreply.github.com>
2024-09-12 11:53:39 +0530

    Add files via upload

---
+++ Program8.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+void main(){
+  int start=10;
+  int end=1;
+  int product=1;
+  while(start>=end){
+    if(start%2==1){
+      product=product*start;
+    }
+    start--;
+  }
+  stdout.write(product);
+}
---
+++ Program9.dart
@@ -0,0 +1,12 @@
+void main(){
+  int numDays=7;
+  while(numDays>=0){
+    if(numDays==0){
+      print("0 days Assignemnt is Overdue");
+    }else{
+      print("$numDays days remaining");
+    }
+    numDays--;
+  }
+  
+}
